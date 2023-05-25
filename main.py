371c4c48bd020c86a736f9c59965b5d0a5bf02ff
prathagautam <prathagautam40@gmail.com>
2023-05-25 14:57:09 +0200

    Final stage all changes made. More CSS added.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #testuser@gmail.com, pw = test
+import json
 import uuid
 
 from fastapi import FastAPI, Request, Form, Response, Depends, File, UploadFile
 from jinja2 import Template
-from typing import Annotated
+from typing import Annotated, Tuple
 import mysql.connector
 from starlette.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -16,37 +17,55 @@ import mysql.connector
 from mysql.connector import errorcode
 import bcrypt
 from auth_utils import create_access_token, decode_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
+from queue import Queue
+from threading import Lock
 
 
 # from sqlread2 import cursor
 
 app = FastAPI()
 
-try:
-    connection = pymysql.connect(
-        host='localhost',
-        user='pratha',
-        password='12345',
-        database='AdvertisementsDb',
-        cursorclass=pymysql.cursors.DictCursor
 
-    )
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your username or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
+class ConnectionPool:
+    def __init__(self, initial_size=5, **kwargs):
+        self.kwargs = kwargs
 
 
+    def get_connection(self):
+        return pymysql.connect(**self.kwargs)
+
+    def release_connection(self, conn):
+        conn.close()
+
+# Example usage:
+pool = ConnectionPool(
+    host='localhost',
+    user='pratha',
+    password='12345',
+    database='AdvertisementsDb',
+    cursorclass=pymysql.cursors.DictCursor,
+    connect_timeout=30
+)
 
 
-def query_db(query, params=None):
+
+
+
+
+def query_db(query, params=None, commit=False, get_last_id=False):
+    connection = pool.get_connection()
     with connection.cursor() as cursor:
         cursor.execute(query, params)
         result = cursor.fetchall()
-        return result
+        if get_last_id:
+            cursor.execute("SELECT LAST_INSERT_ID();")
+            result = cursor.fetchall()[0]['LAST_INSERT_ID()']
+    if commit:
+        connection.commit()
+
+    pool.release_connection(connection)
+    return result
+
 
 
 # Set the directory containing the templates
@@ -65,8 +84,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}  # dictionary, this will be automatically converted to json
+async def root():
+    return RedirectResponse("/home")
 
 
 # to run this,type command uvicorn main:app --reload
@@ -141,10 +160,10 @@ def get_email_from_token(access_token: str) -> str:
     token_and_email = decode_access_token(access_token)
     return token_and_email['sub']
 
-def get_username_and_id_by_email(email:str) -> str:
+def get_username_and_id_by_email(email:str) -> Tuple[str, str]:
     # email = get_email_from_token(access_token)
     user_params = (email,)
-    result = query_db("SELECT FirstName, IdUser from users where email = %s", user_params)
+    result = query_db("SELECT FirstName, IdUser from users where Email = %s", user_params)
     username = result[0]['FirstName']
     user_id = result[0]['IdUser']
     return username, user_id
@@ -159,8 +178,17 @@ async def details(request: Request):
     # template = env.get_template("detailsOfAds.html")
     ad_id = request.query_params["id"]
     user_params = (ad_id,)
-    result = query_db("SELECT * from ads where idad = %s", user_params)
+    result = query_db("SELECT * from ads WHERE idad = %s", user_params)
     access_token = request.cookies.get("access_token")
+    if not result:
+        return templates.TemplateResponse("404.html",
+                                          {"request": request}, status_code=404)
+    user_id_from_ad = result[0]['userID']
+    ad_email = None
+    if user_id_from_ad:
+        user_params = (user_id_from_ad,)
+        ad_email = query_db("SELECT Email from users WHERE IDUser=%s", user_params)
+        ad_email = ad_email[0]['Email']
 
     if access_token:
         email = get_email_from_token(access_token)
@@ -169,10 +197,8 @@ async def details(request: Request):
         email = None
         username = None
         user_id = None
-    if not result:
-        return templates.TemplateResponse("404.html",
-                                          {"request": request, "username": username}, status_code=404)
-    print(result)
+
+    #print(result)
     user_id_from_ad = result[0]['userID']
 
     if user_id == user_id_from_ad:
@@ -180,15 +206,9 @@ async def details(request: Request):
     print(f"DETAILS {user_id=} {user_id_from_ad=}")
 
     return templates.TemplateResponse("detailsofAds.html",
-                                      {"request": request, "username": username,"email":email, "data": result[0], "opened_by_owner":opened_by_owner})
+                                      {"request": request, "username": username,"email":email,"ad_email":ad_email, "data": result[0], "opened_by_owner":opened_by_owner})
     # html_content = template.render(data=result[0])
     # return HTMLResponse(content=html_content, status_code=200)
-
-
-
-
-
-
 
 
 
@@ -245,10 +265,10 @@ def form_post(request: Request,
                         f"VALUES (%s, %s, %s, %s)")
             user_data = (first_name, last_name, email, hashed)
 
-            user_data = query_db(add_user, user_data)
+            user_data = query_db(add_user, user_data, commit=True)
             print(f"This is the user data afetr sign up: {user_data}")
             print(type(user_data))
-            connection.commit()
+
 
             # Return a success message to the user
             # access_token = create_access_token({"sub": email})
@@ -267,10 +287,15 @@ def form_post(request: Request,
 
 
     # request = fastapi.Request(scope=request.scope)
-    return templates.TemplateResponse('sign_up_page.html', {'request': request,"first_name":first_name,"firstname_error":firstname_error, "last_name":last_name,"lastname_error":lastname_error,
-                                                            "email": email, "email_error": email_error,
-                                                            "password": password, "password_error": password_error,
-                                                            "repeat_password": repeat_password, "repeat_password_error": repeat_password_error})
+    return templates.TemplateResponse('sign_up_page.html', {'request': request,
+                                                            "first_name":first_name,
+                                                            "firstname_error":firstname_error,
+                                                            "last_name":last_name,
+                                                            "lastname_error":lastname_error,
+                                                            "email": email,
+                                                            "email_error": email_error,
+                                                            "password_error": password_error,
+                                                            "repeat_password_error": repeat_password_error})
 
 
 
@@ -330,12 +355,8 @@ async def upload_ad(
         user_id,
     )
 
-    result = query_db(insert_query, insert_values)
-    print(result)
-    connection.commit()
-    ad_id = query_db("SELECT LAST_INSERT_ID();")[0]['LAST_INSERT_ID()']
-    print("ad_id:")
-    print(ad_id)
+    ad_id = query_db(insert_query, insert_values, commit=True, get_last_id=True)
+
 
     response = RedirectResponse(url=f"/details?id={ad_id}", status_code=303)
     return response
@@ -392,7 +413,8 @@ def login(request: Request,password: str = Form(""),email: str = Form("")):
         )
         return response
     else:
-        return "WRONG PASSWORD"
+        login_error = "Incorrect email or password!"
+        return templates.TemplateResponse("login_page.html", {"request": request, "login_error": login_error})
 
 
 @app.get("/logout")
@@ -407,9 +429,7 @@ def deleting_ad(ad_id:int, response: Response, request: Request):
     user_params = (ad_id,)
     result = query_db("SELECT * from ads where idad = %s", user_params)
     access_token = request.cookies.get("access_token")
-
-
-
+    
     if access_token:
         email = get_email_from_token(access_token)
         username, user_id = get_username_and_id_by_email(email)
@@ -419,12 +439,155 @@ def deleting_ad(ad_id:int, response: Response, request: Request):
 
         user_id_from_ad = result[0].get('userID')
         if user_id == user_id_from_ad:
-            query_db("DELETE FROM ads where idad = %s", user_params)
-            connection.commit()
+            # getting filename of photo :
+            photo_filename = result[0].get('photo')
+            query_db("DELETE FROM ads where idad = %s", user_params, commit=True)
+
+            #deleting the photo from uploads :
+            if photo_filename:
+                photo_path = os.path.join("uploads",photo_filename)
+                if os.path.exists(photo_path):
+                    os.remove(photo_path)
+
             response = RedirectResponse(url="/products", status_code=303)
         else:
             response = RedirectResponse(url="/forbidden.html", status_code=403)
     return response
+
+
+# Edit ad :
+@app.get("/editablepage/{ad_id}")
+def edit_ad(ad_id: int, request: Request):
+    user_params = (ad_id,)
+    result = query_db("SELECT * from ads where idad = %s", user_params)
+    access_token = request.cookies.get("access_token")
+
+    if access_token is not None:
+        email = get_email_from_token(access_token)
+        username, user_id = get_username_and_id_by_email(email)
+        user_params = (ad_id,)
+        user_id_from_ad = result[0].get('userID')
+        if user_id == user_id_from_ad:
+            # result = query_db("SELECT * FROM ads WHERE idad=%s", user_params)
+                return templates.TemplateResponse("editablepage.html",
+                                                  {"request": request, "username": username, "data": result[0]})
+        if not result:
+            return templates.TemplateResponse("404.html",
+                                              {"request": request, "username": username}, status_code=404)
+    else:
+        return RedirectResponse(url=f"/forbidden.html", status_code=403)
+
+
+@app.post("/editablepage/{ad_id}")
+def editing_ad(
+    ad_id: int,
+    response: Response,
+    request: Request,
+    title: str = Form(...),
+    photo: UploadFile = File(None),
+    price: float = Form(...),
+    negotiation: bool = Form(False),
+    condition: str = Form(...),
+    description: str = Form(...),
+    #phone: str = Form(...)
+):
+    user_params = (ad_id,)
+    result = query_db("SELECT * from ads WHERE idad = %s", user_params)
+    print(result)
+    access_token = request.cookies.get("access_token")
+    if access_token:
+        email = get_email_from_token(access_token)
+        username, user_id = get_username_and_id_by_email(email)
+        if not result:
+            return templates.TemplateResponse(
+                "404.html", {"request": request, "username": username}, status_code=404
+            )
+        user_id_from_ad = result[0].get("userID")
+        if user_id == user_id_from_ad:
+            # Update the ad only if a field has been updated
+            title = title.strip()
+            price = round(price, 2)
+            condition = condition.strip()
+            description = description.strip()
+            photo_name = result[0]["photo"]
+            photo_data = photo.file.read()
+            #phone = phone.strip()
+            if (
+                title != result[0]["title"]
+                or price != result[0]["price"]
+                or negotiation != result[0]["negotiation"]
+                or condition != result[0]["condition"]
+                or description != result[0]["description"]
+                #or phone != result[0]["phone"]
+                or photo_data
+            ):
+                # Save the new photo if provided
+                if photo_data:
+                    # Generate a random file name for the uploaded photo
+                    # file_name, file_extension = os.path.splitext(photo.filename)
+                    # photo_name = f"{ad_id}{file_extension}"
+
+                    # photo_path = os.path.join(f"uploads/{photo_name}")
+                    photo_path = os.path.join("uploads", photo_name)
+
+                    with open(photo_path, "wb") as f:
+                        f.write(photo_data)
+                    photo.file.close()
+                # Update the ad
+                query_db(
+                    "UPDATE ads SET title = %s, photo = %s, price = %s, negotiation = %s, `condition` = %s, description = %s WHERE idad = %s",
+                    (
+                        title,
+                        photo_name,
+                        price,
+                        negotiation,
+                        condition,
+                        description,
+                        ad_id,
+                    ), commit=True
+                )
+            response = RedirectResponse(url=f"/details?id={ad_id}", status_code=303)
+        else:
+            response = RedirectResponse(url="/forbidden.html", status_code=403)
+    else:
+        response = RedirectResponse(url="/login", status_code=303)
+    return response
+
+@app.get("/myads")  # getting only ads of the same logged in user
+async def read_ad(request: Request):
+    access_token = request.cookies.get("access_token")
+    username = None
+    # result = query_db("SELECT * from ads")
+    # access_token = request.cookies.get("access_token")
+    username = None
+    if access_token is not None:
+        email = get_email_from_token(access_token)
+        username, user_id = get_username_and_id_by_email(email)
+        result = query_db("SELECT * FROM ads WHERE userID=%s", (user_id,))
+    else:
+        result = []
+    return templates.TemplateResponse("displayingads.html",
+                                      {"request": request, "username": username, "data": result})
+
+
+@app.get("/searchads")
+def match_keywords(keywords:str, request:Request):
+    query = f"SELECT * FROM ads WHERE title LIKE '%{keywords}%' OR description LIKE '%{keywords}%'"
+    results = query_db(query)
+    print(f"Results : {results}")
+
+    access_token = request.cookies.get("access_token")
+    username=None
+    if access_token is not None:
+        email = get_email_from_token(access_token)
+        username, user_id = get_username_and_id_by_email(email)
+
+    # filtering results :
+    matched_ads = [ad for ad in results if keywords.lower() in ad["title"].lower() or keywords.lower() in ad["description"].lower()]
+    print(f"Matched ads : {matched_ads}")
+
+    return templates.TemplateResponse("displayingads.html",
+                                      {"request": request,"username":username,"data": matched_ads})
 
 
 
--- static/script.js
+++ static/script.js
@@ -19,14 +19,82 @@ function toggleDetails(id)
 }
 
 //function for delete ad button :
-function deleteAd(ad_id)
+async function deleteAd(ad_id)
 {
-    let response =  fetch('/delete_ad/' + ad_id,
+    let response = await fetch('/delete_ad/' + ad_id,
         {
             method: 'POST',
         });
-    if(response.redirected){window.location.href =response.headers.location;}
+    if (response.redirected) {
+    window.location.href = '/products';
+  }
 
 }
+
+function editAd(ad_id)
+{
+    console.log("edit ad function.")
+    let response = fetch('/editablepage/' + ad_id,
+        {
+            method: 'GET',
+        });
+
+}
+function editAd(ad_id)
+{
+    console.log("edit ad function.")
+    window.location.href = '/editablepage/' + ad_id;
+}
+
+function updateChanges(ad_id)
+{
+    const formData = new FormData(document.getElementById("update_changes_form"));
+
+   //sending put request to server with updated data
+    fetch(`/editablepage/${ad_id}`,
+        {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            if(response.redirected)
+            {
+                window.location.href = response.url;
+            } else {
+                console.error('Failed to update ad:', response.statusText);
+            }
+        })
+        .catch(error => console.error('Error:',error));
+}
+
+
+const buttons = document.querySelectorAll('.active');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    buttons.forEach((button) => {
+      button.classList.remove('active');
+    });
+    button.classList.add('active');
+  });
+});
+
+
+// function activateButton(button) {
+//   // remove active class from all buttons
+//   const buttons = document.querySelectorAll('.topnav ul li a');
+//   buttons.forEach(function(btn) {
+//     btn.classList.remove('active');
+//   });
+//
+//   // add active class to clicked button
+//   button.classList.add('active');
+// }
+
+
+
+
+
+
 
 
